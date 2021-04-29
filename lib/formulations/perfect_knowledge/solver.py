7f9e9077817a96e0b62ae3e3034e7b2752c0ec7a
Alkis Plaskovitis <alkisplas@gmail.com>
2021-04-29 17:02:53 +0300

    Add test data for rcpsp_con

--- lib/formulations/perfect_knowledge/solver.py
+++ lib/formulations/perfect_knowledge/solver.py
@@ -3,7 +3,7 @@ import pyomo.environ as pyo
 import pyomo.dataportal as dp
 
 data = dp.DataPortal()
-data.load(filename='test_data.json')
+data.load(filename='rcpsp_con_test_data.json')
 
 instance = rcpsp.model.create_instance(data)
 instance.pprint()
