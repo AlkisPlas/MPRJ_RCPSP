7e585def0a68ea38afb6e304bc8984cf9ad3c7b5
AlkisPlas <alkisplas@gmail.com>
2021-06-12 13:50:36 +0300

    Restructure perfect knowledge solvers

--- rcpsp/util/instance_execution/run_instances.sh
+++ rcpsp/util/instance_execution/run_instances.sh
@@ -8,6 +8,6 @@ for indir in ${instancebase}/json/*; do
     for infile in ${indir}/*.json; do
         instance=$(echo $infile | sed 's:.*/::' | sed "s/\..*//")
         indir=$(echo $indir | sed 's:.*/::')
-        gtimeout 300 python3 ${solverbase}/solver_dis.py ${indir} ${instance}
+        python3 ${solverbase}/discrete/solver_dis.py ${indir} ${instance}
     done
 done
